863d504ab02651b55911d1b7d318d65248b598cd
Mariana Cousseiro de Lima <limatainer@MacBook-Pro-de-Mariana.local>
2023-01-17 20:42:48 +0000

    about us

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,17 @@ import React, { useContext } from 'react'
 import { DarkModeContext } from './util/DarkModeToggle'
 import Services from "./components/Services"
 import Footer from "./components/Footer"
+import About from "./components/About"
+import Description from "./components/Description"
 function App() {
   const { darkMode } = useContext(DarkModeContext)
   return (
     <div className="App">
       <div className={`${darkMode ? 'bg-gray-800' : 'bg-white'} text-${darkMode ? 'white' : 'black'}`}>
-        {/* Your content here */}
       </div>
       <Header />
+      <About />
+      <Description />
       <Services />
       <Footer />
     </div>
---
+++ frontend/src/components/Description.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Explosion from '../assets/img/explosion.svg'
+export default function Description() {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <div className="bg-indigo-50 rounded-md shadow-md p-6">
+        <h1 className="text-center text-zinc-900 text-xl font-medium">About Us</h1>
+        <p className="container text-zinc-900 text-center p-3 m-5">
+          At Elph Digital Solutions, we are committed to providing the best possible service and results for our clients. We understand the importance of a strong online presence and we strive to help businesses succeed in today's digital world. If you're looking for a reliable and experienced digital solutions provider, look no further than Elph Digital Solutions. Contact us today to learn more about how we can help your business thrive!
+        </p>
+      </div>
+    </div>
+  )
+}
